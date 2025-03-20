818056f8c6675e0480ede8ae5e20b714a15df4c8
talalzubair123 <talalmirza317@gmail.com>
2025-03-20 09:33:16 +0500

    Create list of household items.cs

---
+++ list of household items.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> householdItems = new List<string>
+        {
+            "Table",
+            "Chair",
+            "Lamp",
+            "Television",
+            "Fan"
+        }
